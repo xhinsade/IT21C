449d1b87a006b351fed4583cd344dbf43f8a85a9
xhinsade <167082524+xhinsade@users.noreply.github.com>
2024-09-06 15:38:29 +0000

    Added Data Type Displaying Info

--- app.js
+++ app.js
@@ -1,3 +1,17 @@
-console.log("Good afternoon");
-console.log("Object Oriented Programming");
-console.log("Practicing javascript");
+const Info = {
+    Name: "Kristine Lopez",
+    Nickname: "Xhinsade",
+    Hobbies: " Playing online games, Arts, and Listening to Music",
+    Dream: "Unsure ",
+}
+
+console.log("Name:" + Info.Name);
+console.log("Nickname: " + Info.Nickname);
+console.log("Hobbies:" + Info.Hobbies);
+console.log("Dream: " + Info.Dream);
+
+    
+    
+
+
+
