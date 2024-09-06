bf22789df4f52cdf8eb41323bb8c0bf9b865c54e
xhinsade <167082524+xhinsade@users.noreply.github.com>
2024-09-06 17:12:54 +0000

    Added Conditional statements

---
+++ Act3.js
@@ -0,0 +1,40 @@
+//Conditional Statements 
+
+//if-else 
+
+const color = "blue";
+
+if (color == "blue"){
+    console.log("I miss you mwah");
+
+    } else {
+        console.log(" Syempre, Gimingaw japon sa imoa");
+
+        }
+    
+
+    //switch statement 
+
+    let foodNumber = 3;
+
+    switch (foodNumber){
+        case 0:
+            console.log("Adobo");
+            break;
+            case 1:
+                console.log("Menudu");
+                break;
+                case 2: 
+                    console.log("Afritada");
+                    break;
+                    case 3:
+                        console.log("Mechado");
+                        break;
+                        default:
+                            console.log("Wala sa choices, kay dili man ikaw ang gipili");
+                            break;
+    }
+    
+
+    
+       
