a6591d144c2462fecc7470f8c4107352730444a6
xhinsade <167082524+xhinsade@users.noreply.github.com>
2024-09-06 17:46:40 +0000

    Adding the loops

---
+++ Act4.js
@@ -0,0 +1,44 @@
+//Loops
+
+
+//for loop
+for (let x = 1; x <=3; x++){
+    console.log("ug ako nalang diay?");
+}
+console.log("----------------");
+
+// while loop
+let r = 1;
+
+while(r <3 ){
+    console.log("Pwede ako nalang??");
+   r++; 
+}
+console.log("------------------");
+
+//do while
+
+let m = 10;
+do {
+    console.log("Final na gud? ako nalang beh");
+    m--;
+}
+while (m >= 8);
+console.log("--------------------");
+
+//for loop using array
+
+const foods = [ "Potato Fries", "Ice Cream", "Cake"];
+
+
+ for (const foodies of foods) {
+    console.log(foodies)
+   }
+    
+
+
+
+
+
+
+
