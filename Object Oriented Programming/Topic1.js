cd2433c42082bd06cbb24d28e19473af8afe6833
xhinsade <nekochii57@gmail.com>
2024-10-03 13:54:24 +0800

    Added the Output and Update the Label

---
+++ Object Oriented Programming/Topic1.js
@@ -0,0 +1,25 @@
+
+        //Input
+
+             const person1 = {
+             name:"Xhinsade Riri",
+             age:20,
+             status:" Tired but Trying Hard"
+}
+
+        //Process 
+
+         //Display the Person Attribute in the Console
+         console.log(" Name:" + person1.name);
+         console.log(" Age:" + person1.age);
+         console.log(" Name:" + person1.status);
+
+         //Output
+
+         /* 
+
+          Name:Xhinsade Riri
+          Age:20
+          Name: Tired but Trying Hard
+         
+         */
