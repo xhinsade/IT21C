4f4b0e298219b59e07ab77102ecdbb205d1538a7
xhinsade <nekochii57@gmail.com>
2024-10-03 19:46:15 +0800

    Added the Console Process

--- Topic 2 Method/method.js
+++ Topic 2 Method/method.js
@@ -4,6 +4,13 @@ const person ={
     name:"Xhinsade Riri",
     age:20,
     status:" Tired"
-
 }
+
+//Process and Output
+console.log("Name: "+person.name);
+console.log("Age: "+person.age);
+console.log("Status: "+person.status);
+console.log("--------------------------------");
+
+
 
