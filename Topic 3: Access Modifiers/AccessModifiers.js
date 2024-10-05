591a1836afec4854dfdc9c278765088eccada318
xhinsade <nekochii57@gmail.com>
2024-10-05 22:00:20 +0800

    Added the Class and Constructor

---
+++ Topic 3: Access Modifiers/AccessModifiers.js
@@ -0,0 +1,13 @@
+class Animal {
+  
+  #species;
+  #type;
+  #sound;
+  
+  constructor(species, type, sound){
+    this.#species = species;
+    this.#type = type;
+    this.#sound = sound;
+  }
+  
+}
