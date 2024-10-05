c46f2532c1a9b5b07ca446373ddd4cea841857ad
xhinsade <nekochii57@gmail.com>
2024-10-06 00:39:34 +0800

    Added the Method to display Animal Info through HTML

--- Topic 3: Access Modifiers/AccessModifiers.js
+++ Topic 3: Access Modifiers/AccessModifiers.js
@@ -1,6 +1,6 @@
 class Animal {
-  
   /*
+  
   Private Fields: Use to encapsulate data and doesn't allow to access the class properties outside the class itself
   
   */
@@ -15,11 +15,12 @@ class Animal {
     this.#sound = sound;
   }
   
-  /* 
-  Getters: It's a method use to access the value properties in a controlled way. 
-  Return: It's use to return the specify value of a function.
+  /*
   
+  Getters: It's a method use to access the value properties in a controlled way.
+  Return: It's use to return the specify value of a function.
   */
+  
   get species (){
     return this.#species;
   }
@@ -31,11 +32,29 @@ class Animal {
   get sound(){
     return this.#sound;
   }
-  //Method to Display Animal Info in the console 
+
+  // Method to Display Animal Info in the console 
   DisplayAnimalInfo(){
-    
-    console.log(" Species: " + this.species);
-    console.log(" Type: " + this.type);
+    console.log("Species: " + this.species);
+    console.log("Type: " + this.type);
     console.log("Sound: " + this.sound);
   }
+
+  // Method to Display Animal Info in HTML
+  DisplayAnimalInfo_Html(){
+    const AnimalElement = document.getElementById('Info');
+    AnimalElement.innerHTML = `
+      <strong>Species:</strong> ${this.species}<br>
+      <strong>Type:</strong> ${this.type}<br>
+      <strong>Sound:</strong> ${this.sound}
+    `;
+  }
 }
+
+// Creating instances of the Animal class
+const animalInfo1 = new Animal('Mammal', 'Cat', 'Meow');
+const animalInfo2 = new Animal('Vertebrate', 'Crocodile', 'grunt');
+
+// Call methods to display info
+animalInfo1.DisplayAnimalInfo();
+animalInfo2.DisplayAnimalInfo();
