e28134ce22ffb4388040f1f6c78799e8673e2194
xhinsade <nekochii57@gmail.com>
2024-10-15 20:59:08 +0800

    Added the fetch constructor

--- Topic 5: Bar Chart/chart.js
+++ Topic 5: Bar Chart/chart.js
@@ -11,4 +11,16 @@ class ChartCreator {
             this.createCharts();
         }
     }
+    
+    async fetchData() {
+        try {
+            const response = await fetch(this.dataUrl);
+            if (!response.ok) {
+                throw new Error('Network response was not ok ' + response.statusText);
+            }
+            this.chartData = await response.json();
+        } catch (error) {
+            console.error('There has been a problem with your fetch operation:', error);
+        }
+    }
 }
