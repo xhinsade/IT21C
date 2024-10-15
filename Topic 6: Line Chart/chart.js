ef643b1f3fd801f035a438ba946a345867fd9852
xhinsade <nekochii57@gmail.com>
2024-10-15 22:34:00 +0800

    Fetch the data from the JSON file

--- Topic 6: Line Chart/chart.js
+++ Topic 6: Line Chart/chart.js
@@ -13,3 +13,16 @@ class LineChart {
             this.createChart();
         }
     }
+    
+    // Fetch the data from the JSON file
+    async fetchData() {
+        try {
+            const response = await fetch(this.dataUrl);
+            if (!response.ok) {
+                throw new Error('Network response was not ok ' + response.statusText);
+            }
+            this.chartData = await response.json();
+        } catch (error) {
+            console.error('There has been a problem with your fetch operation:', error);
+        }
+    }
