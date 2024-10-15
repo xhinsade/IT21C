ef79ab65c804ff11ab54a19ff9d3b70eb66918b5
xhinsade <nekochii57@gmail.com>
2024-10-15 20:59:50 +0800

    Method to create chart

--- Topic 5: Bar Chart/chart.js
+++ Topic 5: Bar Chart/chart.js
@@ -23,4 +23,9 @@ class ChartCreator {
             console.error('There has been a problem with your fetch operation:', error);
         }
     }
+    
+    createCharts() {
+        // This method will be overridden in subclasses
+        throw new Error('createCharts() must be implemented in subclasses');
+    }
 }
